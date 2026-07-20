a0836b0a5115965dd3fb3bd177c1189140d056a1
WFTurnip <tomohiro.kabura@icloud.com>
2026-07-20 21:27:17 +0900

    update

--- creative/fonts/generate.sh
+++ creative/fonts/generate.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
-
 echo "フォント生成スクリプトへようこそ。"
 echo "以下のオプションから選択してください："
 echo "1. each - 個別のフォントファイルを指定して生成"
 echo "2. all  - ディレクトリ内のすべてのフォントファイルを生成"
 echo "オプションを入力してください（each / all）："
-
 read -p ">>> " option
-
 case $option in
 each)
     echo "生成するフォントのファイル名を入力してください。"
     ls -d *.sfd
     read -p ">>> " fontname
-
     fontforge -script generate.ff "${fontname}"
-
     echo "フォント${fontname}の生成が完了しました。"
     ;;
 all)
     for fontfile in *.sfd; do
-        fontforge -script generate.ff ${fontfile}
+        fontforge -script generate.ff "${fontfile}"
         echo "${fontfile}からのフォント生成が完了しました。"
     done
     ;;
--- creative/fonts/variants_consonants_concatenator.sh
+++ creative/fonts/variants_consonants_concatenator.sh
@@ -5,76 +5,76 @@ ls -d */
 read fontname
 
 # isolate form -----------------------------------------------------------------
-cat $fontname/uni004b.isol.svg $fontname/uni005c.svg >$fontname/uni0047.isol.svg
-cat $fontname/uni0054.isol.svg $fontname/uni005c.svg >$fontname/uni0044.isol.svg
-cat $fontname/uni0053.isol.svg $fontname/uni005c.svg >$fontname/uni005a.isol.svg
-cat $fontname/uni0051.isol.svg $fontname/uni005c.svg >$fontname/uni0043.isol.svg
-cat $fontname/uni0052.isol.svg $fontname/uni005c.svg >$fontname/uni004c.isol.svg
-cat $fontname/uni0050.isol.svg $fontname/uni005c.svg >$fontname/uni0042.isol.svg
-cat $fontname/uni0048.isol.svg $fontname/uni005c.svg >$fontname/uni0058.isol.svg
-cat $fontname/uni0046.isol.svg $fontname/uni005c.svg >$fontname/uni0056.isol.svg
-cat $fontname/uni004d.isol.svg $fontname/uni005c.svg >$fontname/uni004e.isol.svg
-cat $fontname/uni0053.isol.svg $fontname/uni005c.svg $fontname/uni0030.svg >$fontname/uni004a.isol.svg
-cat $fontname/uni0051.isol.svg $fontname/uni005c.svg $fontname/uni0030.svg >$fontname/uni0059.isol.svg
+cat "$fontname"/uni004b.isol.svg "$fontname"/uni005c.svg >"$fontname"/uni0047.isol.svg
+cat "$fontname"/uni0054.isol.svg "$fontname"/uni005c.svg >"$fontname"/uni0044.isol.svg
+cat "$fontname"/uni0053.isol.svg "$fontname"/uni005c.svg >"$fontname"/uni005a.isol.svg
+cat "$fontname"/uni0051.isol.svg "$fontname"/uni005c.svg >"$fontname"/uni0043.isol.svg
+cat "$fontname"/uni0052.isol.svg "$fontname"/uni005c.svg >"$fontname"/uni004c.isol.svg
+cat "$fontname"/uni0050.isol.svg "$fontname"/uni005c.svg >"$fontname"/uni0042.isol.svg
+cat "$fontname"/uni0048.isol.svg "$fontname"/uni005c.svg >"$fontname"/uni0058.isol.svg
+cat "$fontname"/uni0046.isol.svg "$fontname"/uni005c.svg >"$fontname"/uni0056.isol.svg
+cat "$fontname"/uni004d.isol.svg "$fontname"/uni005c.svg >"$fontname"/uni004e.isol.svg
+cat "$fontname"/uni0053.isol.svg "$fontname"/uni005c.svg "$fontname"/uni0030.svg >"$fontname"/uni004a.isol.svg
+cat "$fontname"/uni0051.isol.svg "$fontname"/uni005c.svg "$fontname"/uni0030.svg >"$fontname"/uni0059.isol.svg
 # initial form -----------------------------------------------------------------
-cat $fontname/uni004b.init.svg $fontname/uni005c.svg >$fontname/uni0047.init.svg
-cat $fontname/uni0054.init.svg $fontname/uni005c.svg >$fontname/uni0044.init.svg
-cat $fontname/uni0053.init.svg $fontname/uni005c.svg >$fontname/uni005a.init.svg
-cat $fontname/uni0051.init.svg $fontname/uni005c.svg >$fontname/uni0043.init.svg
-cat $fontname/uni0052.init.svg $fontname/uni005c.svg >$fontname/uni004c.init.svg
-cat $fontname/uni0050.init.svg $fontname/uni005c.svg >$fontname/uni0042.init.svg
-cat $fontname/uni0048.init.svg $fontname/uni005c.svg >$fontname/uni0058.init.svg
-cat $fontname/uni0046.init.svg $fontname/uni005c.svg >$fontname/uni0056.init.svg
-cat $fontname/uni004d.init.svg $fontname/uni005c.svg >$fontname/uni004e.init.svg
-cat $fontname/uni0053.init.svg $fontname/uni005c.svg $fontname/uni0030.svg >$fontname/uni004a.init.svg
-cat $fontname/uni0051.init.svg $fontname/uni005c.svg $fontname/uni0030.svg >$fontname/uni0059.init.svg
+cat "$fontname"/uni004b.init.svg "$fontname"/uni005c.svg >"$fontname"/uni0047.init.svg
+cat "$fontname"/uni0054.init.svg "$fontname"/uni005c.svg >"$fontname"/uni0044.init.svg
+cat "$fontname"/uni0053.init.svg "$fontname"/uni005c.svg >"$fontname"/uni005a.init.svg
+cat "$fontname"/uni0051.init.svg "$fontname"/uni005c.svg >"$fontname"/uni0043.init.svg
+cat "$fontname"/uni0052.init.svg "$fontname"/uni005c.svg >"$fontname"/uni004c.init.svg
+cat "$fontname"/uni0050.init.svg "$fontname"/uni005c.svg >"$fontname"/uni0042.init.svg
+cat "$fontname"/uni0048.init.svg "$fontname"/uni005c.svg >"$fontname"/uni0058.init.svg
+cat "$fontname"/uni0046.init.svg "$fontname"/uni005c.svg >"$fontname"/uni0056.init.svg
+cat "$fontname"/uni004d.init.svg "$fontname"/uni005c.svg >"$fontname"/uni004e.init.svg
+cat "$fontname"/uni0053.init.svg "$fontname"/uni005c.svg "$fontname"/uni0030.svg >"$fontname"/uni004a.init.svg
+cat "$fontname"/uni0051.init.svg "$fontname"/uni005c.svg "$fontname"/uni0030.svg >"$fontname"/uni0059.init.svg
 # medial form ------------------------------------------------------------------
-cat $fontname/uni004b.medi.svg $fontname/uni005c.svg >$fontname/uni0047.medi.svg
-cat $fontname/uni0054.medi.svg $fontname/uni005c.svg >$fontname/uni0044.medi.svg
-cat $fontname/uni0053.medi.svg $fontname/uni005c.svg >$fontname/uni005a.medi.svg
-cat $fontname/uni0051.medi.svg $fontname/uni005c.svg >$fontname/uni0043.medi.svg
-cat $fontname/uni0052.medi.svg $fontname/uni005c.svg >$fontname/uni004c.medi.svg
-cat $fontname/uni0050.medi.svg $fontname/uni005c.svg >$fontname/uni0042.medi.svg
-cat $fontname/uni0048.medi.svg $fontname/uni005c.svg >$fontname/uni0058.medi.svg
-cat $fontname/uni0046.medi.svg $fontname/uni005c.svg >$fontname/uni0056.medi.svg
-cat $fontname/uni004d.medi.svg $fontname/uni005c.svg >$fontname/uni004e.medi.svg
-cat $fontname/uni0053.medi.svg $fontname/uni005c.svg $fontname/uni0030.svg >$fontname/uni004a.medi.svg
-cat $fontname/uni0051.medi.svg $fontname/uni005c.svg $fontname/uni0030.svg >$fontname/uni0059.medi.svg
+cat "$fontname"/uni004b.medi.svg "$fontname"/uni005c.svg >"$fontname"/uni0047.medi.svg
+cat "$fontname"/uni0054.medi.svg "$fontname"/uni005c.svg >"$fontname"/uni0044.medi.svg
+cat "$fontname"/uni0053.medi.svg "$fontname"/uni005c.svg >"$fontname"/uni005a.medi.svg
+cat "$fontname"/uni0051.medi.svg "$fontname"/uni005c.svg >"$fontname"/uni0043.medi.svg
+cat "$fontname"/uni0052.medi.svg "$fontname"/uni005c.svg >"$fontname"/uni004c.medi.svg
+cat "$fontname"/uni0050.medi.svg "$fontname"/uni005c.svg >"$fontname"/uni0042.medi.svg
+cat "$fontname"/uni0048.medi.svg "$fontname"/uni005c.svg >"$fontname"/uni0058.medi.svg
+cat "$fontname"/uni0046.medi.svg "$fontname"/uni005c.svg >"$fontname"/uni0056.medi.svg
+cat "$fontname"/uni004d.medi.svg "$fontname"/uni005c.svg >"$fontname"/uni004e.medi.svg
+cat "$fontname"/uni0053.medi.svg "$fontname"/uni005c.svg "$fontname"/uni0030.svg >"$fontname"/uni004a.medi.svg
+cat "$fontname"/uni0051.medi.svg "$fontname"/uni005c.svg "$fontname"/uni0030.svg >"$fontname"/uni0059.medi.svg
 # final form -------------------------------------------------------------------
-cat $fontname/uni004b.fina.svg $fontname/uni005c.svg >$fontname/uni0047.fina.svg
-cat $fontname/uni0054.fina.svg $fontname/uni005c.svg >$fontname/uni0044.fina.svg
-cat $fontname/uni0053.fina.svg $fontname/uni005c.svg >$fontname/uni005a.fina.svg
-cat $fontname/uni0051.fina.svg $fontname/uni005c.svg >$fontname/uni0043.fina.svg
-cat $fontname/uni0052.fina.svg $fontname/uni005c.svg >$fontname/uni004c.fina.svg
-cat $fontname/uni0050.fina.svg $fontname/uni005c.svg >$fontname/uni0042.fina.svg
-cat $fontname/uni0048.fina.svg $fontname/uni005c.svg >$fontname/uni0058.fina.svg
-cat $fontname/uni0046.fina.svg $fontname/uni005c.svg >$fontname/uni0056.fina.svg
-cat $fontname/uni004d.fina.svg $fontname/uni005c.svg >$fontname/uni004e.fina.svg
-cat $fontname/uni0053.fina.svg $fontname/uni005c.svg $fontname/uni0030.svg >$fontname/uni004a.fina.svg
-cat $fontname/uni0051.fina.svg $fontname/uni005c.svg $fontname/uni0030.svg >$fontname/uni0059.fina.svg
-if [ "$fontname" == "sulive" ]; then
+cat "$fontname"/uni004b.fina.svg "$fontname"/uni005c.svg >"$fontname"/uni0047.fina.svg
+cat "$fontname"/uni0054.fina.svg "$fontname"/uni005c.svg >"$fontname"/uni0044.fina.svg
+cat "$fontname"/uni0053.fina.svg "$fontname"/uni005c.svg >"$fontname"/uni005a.fina.svg
+cat "$fontname"/uni0051.fina.svg "$fontname"/uni005c.svg >"$fontname"/uni0043.fina.svg
+cat "$fontname"/uni0052.fina.svg "$fontname"/uni005c.svg >"$fontname"/uni004c.fina.svg
+cat "$fontname"/uni0050.fina.svg "$fontname"/uni005c.svg >"$fontname"/uni0042.fina.svg
+cat "$fontname"/uni0048.fina.svg "$fontname"/uni005c.svg >"$fontname"/uni0058.fina.svg
+cat "$fontname"/uni0046.fina.svg "$fontname"/uni005c.svg >"$fontname"/uni0056.fina.svg
+cat "$fontname"/uni004d.fina.svg "$fontname"/uni005c.svg >"$fontname"/uni004e.fina.svg
+cat "$fontname"/uni0053.fina.svg "$fontname"/uni005c.svg "$fontname"/uni0030.svg >"$fontname"/uni004a.fina.svg
+cat "$fontname"/uni0051.fina.svg "$fontname"/uni005c.svg "$fontname"/uni0030.svg >"$fontname"/uni0059.fina.svg
+if [ ""$fontname"" == "sulive" ]; then
     # second medial form -----------------------------------------------------------
-    cat $fontname/uni004b.med2.svg $fontname/uni005c.svg >$fontname/uni0047.med2.svg
-    cat $fontname/uni0054.med2.svg $fontname/uni005c.svg >$fontname/uni0044.med2.svg
-    cat $fontname/uni0053.med2.svg $fontname/uni005c.svg >$fontname/uni005a.med2.svg
-    cat $fontname/uni0051.med2.svg $fontname/uni005c.svg >$fontname/uni0043.med2.svg
-    cat $fontname/uni0052.med2.svg $fontname/uni005c.svg >$fontname/uni004c.med2.svg
-    cat $fontname/uni0050.med2.svg $fontname/uni005c.svg >$fontname/uni0042.med2.svg
-    cat $fontname/uni0048.med2.svg $fontname/uni005c.svg >$fontname/uni0058.med2.svg
-    cat $fontname/uni0046.med2.svg $fontname/uni005c.svg >$fontname/uni0056.med2.svg
-    cat $fontname/uni004d.med2.svg $fontname/uni005c.svg >$fontname/uni004e.med2.svg
-    cat $fontname/uni0053.med2.svg $fontname/uni005c.svg $fontname/uni0030.svg >$fontname/uni004a.med2.svg
-    cat $fontname/uni0051.med2.svg $fontname/uni005c.svg $fontname/uni0030.svg >$fontname/uni0059.med2.svg
+    cat "$fontname"/uni004b.med2.svg "$fontname"/uni005c.svg >"$fontname"/uni0047.med2.svg
+    cat "$fontname"/uni0054.med2.svg "$fontname"/uni005c.svg >"$fontname"/uni0044.med2.svg
+    cat "$fontname"/uni0053.med2.svg "$fontname"/uni005c.svg >"$fontname"/uni005a.med2.svg
+    cat "$fontname"/uni0051.med2.svg "$fontname"/uni005c.svg >"$fontname"/uni0043.med2.svg
+    cat "$fontname"/uni0052.med2.svg "$fontname"/uni005c.svg >"$fontname"/uni004c.med2.svg
+    cat "$fontname"/uni0050.med2.svg "$fontname"/uni005c.svg >"$fontname"/uni0042.med2.svg
+    cat "$fontname"/uni0048.med2.svg "$fontname"/uni005c.svg >"$fontname"/uni0058.med2.svg
+    cat "$fontname"/uni0046.med2.svg "$fontname"/uni005c.svg >"$fontname"/uni0056.med2.svg
+    cat "$fontname"/uni004d.med2.svg "$fontname"/uni005c.svg >"$fontname"/uni004e.med2.svg
+    cat "$fontname"/uni0053.med2.svg "$fontname"/uni005c.svg "$fontname"/uni0030.svg >"$fontname"/uni004a.med2.svg
+    cat "$fontname"/uni0051.med2.svg "$fontname"/uni005c.svg "$fontname"/uni0030.svg >"$fontname"/uni0059.med2.svg
     # second final form ------------------------------------------------------------
-    cat $fontname/uni004b.fin2.svg $fontname/uni005c.svg >$fontname/uni0047.fin2.svg
-    cat $fontname/uni0054.fin2.svg $fontname/uni005c.svg >$fontname/uni0044.fin2.svg
-    cat $fontname/uni0053.fin2.svg $fontname/uni005c.svg >$fontname/uni005a.fin2.svg
-    cat $fontname/uni0051.fin2.svg $fontname/uni005c.svg >$fontname/uni0043.fin2.svg
-    cat $fontname/uni0052.fin2.svg $fontname/uni005c.svg >$fontname/uni004c.fin2.svg
-    cat $fontname/uni0050.fin2.svg $fontname/uni005c.svg >$fontname/uni0042.fin2.svg
-    cat $fontname/uni0048.fin2.svg $fontname/uni005c.svg >$fontname/uni0058.fin2.svg
-    cat $fontname/uni0046.fin2.svg $fontname/uni005c.svg >$fontname/uni0056.fin2.svg
-    cat $fontname/uni004d.fin2.svg $fontname/uni005c.svg >$fontname/uni004e.fin2.svg
-    cat $fontname/uni0053.fin2.svg $fontname/uni005c.svg $fontname/uni0030.svg >$fontname/uni004a.fin2.svg
-    cat $fontname/uni0051.fin2.svg $fontname/uni005c.svg $fontname/uni0030.svg >$fontname/uni0059.fin2.svg
+    cat "$fontname"/uni004b.fin2.svg "$fontname"/uni005c.svg >"$fontname"/uni0047.fin2.svg
+    cat "$fontname"/uni0054.fin2.svg "$fontname"/uni005c.svg >"$fontname"/uni0044.fin2.svg
+    cat "$fontname"/uni0053.fin2.svg "$fontname"/uni005c.svg >"$fontname"/uni005a.fin2.svg
+    cat "$fontname"/uni0051.fin2.svg "$fontname"/uni005c.svg >"$fontname"/uni0043.fin2.svg
+    cat "$fontname"/uni0052.fin2.svg "$fontname"/uni005c.svg >"$fontname"/uni004c.fin2.svg
+    cat "$fontname"/uni0050.fin2.svg "$fontname"/uni005c.svg >"$fontname"/uni0042.fin2.svg
+    cat "$fontname"/uni0048.fin2.svg "$fontname"/uni005c.svg >"$fontname"/uni0058.fin2.svg
+    cat "$fontname"/uni0046.fin2.svg "$fontname"/uni005c.svg >"$fontname"/uni0056.fin2.svg
+    cat "$fontname"/uni004d.fin2.svg "$fontname"/uni005c.svg >"$fontname"/uni004e.fin2.svg
+    cat "$fontname"/uni0053.fin2.svg "$fontname"/uni005c.svg "$fontname"/uni0030.svg >"$fontname"/uni004a.fin2.svg
+    cat "$fontname"/uni0051.fin2.svg "$fontname"/uni005c.svg "$fontname"/uni0030.svg >"$fontname"/uni0059.fin2.svg
 fi
