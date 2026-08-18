d8b98a3270ed755ed073161a4811e709b3181db1
WFTurnip <95839021+WFTurnip@users.noreply.github.com>
2026-08-18 17:47:18 +0900

    infomation.sh を更新

--- creative/fonts/infomation.sh
+++ creative/fonts/infomation.sh
@@ -13,14 +13,14 @@ case $option in
         ls -d *.sfd
         read -p ">>> " fontname
         echo "${fontfile}のフォント情報を出力します。"
-        fontforge -script infomation.ff "${fontname}"
+        fontforge -script information.ff "${fontname}"
         echo "${fontname}のフォント情報出力が完了しました。"
         ;;
     all)
         for fontfile in *.sfd; do
             echo "======================================"
             echo "${fontfile}のフォント情報を出力します。"
-            fontforge -script infomation.ff "${fontfile}"
+            fontforge -script information.ff "${fontfile}"
             echo "${fontfile}のフォント情報出力が完了しました。"
         done
         ;;
