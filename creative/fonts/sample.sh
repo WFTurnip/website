4f9cea087dd3199f2c450168eac93066a86ff41a
WFTurnip <tomohiro.kabura@icloud.com>
2026-09-03 23:53:34 +0900

    update

--- creative/fonts/sample.sh
+++ creative/fonts/sample.sh
@@ -1,24 +1,4 @@
 #!/bin/bash
-# isolate form --------------------
-cp zosokw/uni004b.medi.svg zosokw/uni004b.med2.svg
-cp zosokw/uni0054.medi.svg zosokw/uni0054.med2.svg
-cp zosokw/uni0053.medi.svg zosokw/uni0053.med2.svg
-cp zosokw/uni0051.medi.svg zosokw/uni0051.med2.svg
-cp zosokw/uni0052.medi.svg zosokw/uni0052.med2.svg
-cp zosokw/uni0050.medi.svg zosokw/uni0050.med2.svg
-cp zosokw/uni0048.medi.svg zosokw/uni0048.med2.svg
-cp zosokw/uni0046.medi.svg zosokw/uni0046.med2.svg
-cp zosokw/uni004d.medi.svg zosokw/uni004d.med2.svg
-
-cp zosokw/uni004b.fina.svg zosokw/uni004b.fin2.svg
-cp zosokw/uni0054.fina.svg zosokw/uni0054.fin2.svg
-cp zosokw/uni0053.fina.svg zosokw/uni0053.fin2.svg
-cp zosokw/uni0051.fina.svg zosokw/uni0051.fin2.svg
-cp zosokw/uni0052.fina.svg zosokw/uni0052.fin2.svg
-cp zosokw/uni0050.fina.svg zosokw/uni0050.fin2.svg
-cp zosokw/uni0048.fina.svg zosokw/uni0048.fin2.svg
-cp zosokw/uni0046.fina.svg zosokw/uni0046.fin2.svg
-cp zosokw/uni004d.fina.svg zosokw/uni004d.fin2.svg
 # initial form --------------------
 # open -a /Applications/Visual\ Studio\ Code.app zosokw/uni004b.init.svg
 # open -a /Applications/Visual\ Studio\ Code.app zosokw/uni0054.init.svg
