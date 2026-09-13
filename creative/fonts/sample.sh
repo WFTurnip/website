50db8760f4d684f144693f7d9a0cabcb635dfe51
WFTurnip <tomohiro.kabura@icloud.com>
2026-09-13 23:48:30 +0900

    update

--- creative/fonts/sample.sh
+++ creative/fonts/sample.sh
@@ -1,51 +1,10 @@
 #!/bin/bash
-# initial form --------------
-# open zosokw/uni004b.init.svg
-# open zosokw/uni0054.init.svg
-# open zosokw/uni0053.init.svg
-# open zosokw/uni0051.init.svg
-# open zosokw/uni0052.init.svg
-# open zosokw/uni0050.init.svg
-# open zosokw/uni0048.init.svg
-# open zosokw/uni0046.init.svg
-# open zosokw/uni004d.init.svg
-# medial form ---------------
-# open zosokw/uni004b.medi.svg
-# open zosokw/uni0054.medi.svg
-# open zosokw/uni0053.medi.svg
-# open zosokw/uni0051.medi.svg
-# open zosokw/uni0052.medi.svg
-# open zosokw/uni0050.medi.svg
-# open zosokw/uni0048.medi.svg
-# open zosokw/uni0046.medi.svg
-# open zosokw/uni004d.medi.svg
-# final form ----------------
-# open zosokw/uni004b.fina.svg
-# open zosokw/uni0054.fina.svg
-# open zosokw/uni0053.fina.svg
-# open zosokw/uni0051.fina.svg
-# open zosokw/uni0052.fina.svg
-# open zosokw/uni0050.fina.svg
-# open zosokw/uni0048.fina.svg
-# open zosokw/uni0046.fina.svg
-# open zosokw/uni004d.fina.svg
-# second medial form --------
-# open zosokw/uni004b.med2.svg
-# open zosokw/uni0054.med2.svg
-# open zosokw/uni0053.med2.svg
-# open zosokw/uni0051.med2.svg
-# open zosokw/uni0052.med2.svg
-# open zosokw/uni0050.med2.svg
-# open zosokw/uni0048.med2.svg
-# open zosokw/uni0046.med2.svg
-# open zosokw/uni004d.med2.svg
-# second final form ---------
-# open zosokw/uni004b.fin2.svg
-# open zosokw/uni0054.fin2.svg
-# open zosokw/uni0053.fin2.svg
-# open zosokw/uni0051.fin2.svg
-# open zosokw/uni0052.fin2.svg
-# open zosokw/uni0050.fin2.svg
-# open zosokw/uni0048.fin2.svg
-# open zosokw/uni0046.fin2.svg
-# open zosokw/uni004d.fin2.svg
+open -a /Applications/Visual\ Studio\ Code.app zosokw/uni004b.medi.svg
+open -a /Applications/Visual\ Studio\ Code.app zosokw/uni0054.medi.svg
+open -a /Applications/Visual\ Studio\ Code.app zosokw/uni0053.medi.svg
+open -a /Applications/Visual\ Studio\ Code.app zosokw/uni0051.medi.svg
+open -a /Applications/Visual\ Studio\ Code.app zosokw/uni0052.medi.svg
+open -a /Applications/Visual\ Studio\ Code.app zosokw/uni0050.medi.svg
+open -a /Applications/Visual\ Studio\ Code.app zosokw/uni0048.medi.svg
+open -a /Applications/Visual\ Studio\ Code.app zosokw/uni0046.medi.svg
+open -a /Applications/Visual\ Studio\ Code.app zosokw/uni004d.medi.svg
