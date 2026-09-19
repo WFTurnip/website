865f115c754ef36accfac2c3d7b6952e54c0a17e
WFTurnip <tomohiro.kabura@icloud.com>
2026-09-19 19:08:22 +0900

    update

--- creative/fonts/brakets_maker.sh
+++ creative/fonts/brakets_maker.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-echo "開くフォントのディレクトリ名を入力してください。"
-ls -d */
-read -p ">>> " dirname
-touch "${dirname}"/uni003c.svg
-touch "${dirname}"/uni003e.svg
-touch "${dirname}"/uni0028.svg
-touch "${dirname}"/uni0029.svg
-touch "${dirname}"/uni005b.svg
-touch "${dirname}"/uni005d.svg
-touch "${dirname}"/uni007b.svg
-touch "${dirname}"/uni007d.svg
+fontname=("kodito" "lekuta" "lozegw" "makina" "piswpi" "polwgo" "silwki" "sulive" "xavani" "xesada" "xidili" "zosokw")
+for dirname in "${fontname[@]}"; do
+    echo "ディレクトリ${dirname}の基本文字のタイムスタンプを更新します。"
+    touch "${dirname}"/uni003c.svg
+    touch "${dirname}"/uni003e.svg
+    touch "${dirname}"/uni0028.svg
+    touch "${dirname}"/uni0029.svg
+    touch "${dirname}"/uni005b.svg
+    touch "${dirname}"/uni005d.svg
+    touch "${dirname}"/uni007b.svg
+    touch "${dirname}"/uni007d.svg
+    echo "ディレクトリ${dirname}の基本文字のタイムスタンプを更新しました。"
+done
--- creative/fonts/numerals_maker.sh
+++ creative/fonts/numerals_maker.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
-echo "開くフォントのディレクトリ名を入力してください。"
-ls -d */
-read -p ">>> " dirname
-touch "${dirname}"/uni0030.svg
-# 0
-touch "${dirname}"/uni0031.svg
-# 1
-touch "${dirname}"/uni0032.svg
-# 2
-touch "${dirname}"/uni0033.svg
-# 3
-touch "${dirname}"/uni0034.svg
-# 4
-touch "${dirname}"/uni0035.svg
-# 5
-touch "${dirname}"/uni0036.svg
-# 6
-touch "${dirname}"/uni0037.svg
-# 7
-touch "${dirname}"/uni0038.svg
-# 8
-touch "${dirname}"/uni0039.svg
-# 9
+fontname=("kodito" "lekuta" "lozegw" "makina" "piswpi" "polwgo" "silwki" "sulive" "xavani" "xesada" "xidili" "zosokw")
+for dirname in "${fontname[@]}"; do
+    echo "ディレクトリ${dirname}の基本文字のタイムスタンプを更新します。"
+    touch "${dirname}"/uni0030.svg # 0
+    touch "${dirname}"/uni0031.svg # 1
+    touch "${dirname}"/uni0032.svg # 2
+    touch "${dirname}"/uni0033.svg # 3
+    touch "${dirname}"/uni0034.svg # 4
+    touch "${dirname}"/uni0035.svg # 5
+    touch "${dirname}"/uni0036.svg # 6
+    touch "${dirname}"/uni0037.svg # 7
+    touch "${dirname}"/uni0038.svg # 8
+    touch "${dirname}"/uni0039.svg # 9
+    echo "ディレクトリ${dirname}の基本文字のタイムスタンプを更新しました。"
+done
