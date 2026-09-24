546f66eaab9b40059268111e3204ef4d0bba4ff5
WFTurnip <tomohiro.kabura@icloud.com>
2026-09-24 21:47:42 +0900

    update

--- creative/fonts/vowels_concatenator.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "結合するフォントのディレクトリ名を入力してください。"
-ls -d */
-read -p ">>> " dirname
-cat "${dirname}"/uni0049.svg "${dirname}"/uni0045.svg >"${dirname}"/uni0041.svg #
-cat "${dirname}"/uni0057.svg "${dirname}"/uni0055.svg >"${dirname}"/uni004f.svg #
