17809d1627291079511ce3596047260f95600d12
WFTurnip <tomohiro.kabura@icloud.com>
2026-09-25 22:11:33 +0900

    update

--- creative/fonts/numerals_concatenator.sh
+++ creative/fonts/numerals_concatenator.sh
@@ -2,12 +2,12 @@
 echo "結合するフォントのディレクトリ名を入力してください。"
 ls -d */
 read -p ">>> " dirname
-cat "${dirname}"/uni004b.svg "${dirname}"/uni0030.svg >"${dirname}"/uni0031.svg # k 0 >1
-cat "${dirname}"/uni0054.svg "${dirname}"/uni0030.svg >"${dirname}"/uni0032.svg # t 0 >2
-cat "${dirname}"/uni0053.svg "${dirname}"/uni0030.svg >"${dirname}"/uni0033.svg # s 0 >3
-cat "${dirname}"/uni0051.svg "${dirname}"/uni0030.svg >"${dirname}"/uni0034.svg # q 0 >4
-cat "${dirname}"/uni0052.svg "${dirname}"/uni0030.svg >"${dirname}"/uni0035.svg # r 0 >5
-cat "${dirname}"/uni0050.svg "${dirname}"/uni0030.svg >"${dirname}"/uni0036.svg # p 0 >6
-cat "${dirname}"/uni0048.svg "${dirname}"/uni0030.svg >"${dirname}"/uni0037.svg # h 0 >7
-cat "${dirname}"/uni0046.svg "${dirname}"/uni0030.svg >"${dirname}"/uni0038.svg # f 0 >8
-cat "${dirname}"/uni004d.svg "${dirname}"/uni0030.svg >"${dirname}"/uni0039.svg # m 0 >9
+cat "${dirname}"/uni004b.svg "${dirname}"/uni0030.svg >"${dirname}"/uni0031.svg # CONCATENATE K 0 >1
+cat "${dirname}"/uni0054.svg "${dirname}"/uni0030.svg >"${dirname}"/uni0032.svg # CONCATENATE T 0 >2
+cat "${dirname}"/uni0053.svg "${dirname}"/uni0030.svg >"${dirname}"/uni0033.svg # CONCATENATE S 0 >3
+cat "${dirname}"/uni0051.svg "${dirname}"/uni0030.svg >"${dirname}"/uni0034.svg # CONCATENATE Q 0 >4
+cat "${dirname}"/uni0052.svg "${dirname}"/uni0030.svg >"${dirname}"/uni0035.svg # CONCATENATE R 0 >5
+cat "${dirname}"/uni0050.svg "${dirname}"/uni0030.svg >"${dirname}"/uni0036.svg # CONCATENATE P 0 >6
+cat "${dirname}"/uni0048.svg "${dirname}"/uni0030.svg >"${dirname}"/uni0037.svg # CONCATENATE H 0 >7
+cat "${dirname}"/uni0046.svg "${dirname}"/uni0030.svg >"${dirname}"/uni0038.svg # CONCATENATE F 0 >8
+cat "${dirname}"/uni004d.svg "${dirname}"/uni0030.svg >"${dirname}"/uni0039.svg # CONCATENATE M 0 >9
--- creative/fonts/vowels_maker.sh
+++ creative/fonts/vowels_maker.sh
@@ -2,11 +2,11 @@
 fontname=("kodito" "lekuta" "lozegw" "makina" "piswpi" "polwgo" "silwki" "sulive" "xavani" "xesada" "xidili" "zosokw")
 for dirname in "${fontname[@]}"; do
     echo "ディレクトリ${dirname}の母音符号のタイムスタンプを更新します。"
-    touch "${dirname}"/uni0041.svg
-    touch "${dirname}"/uni0045.svg
-    touch "${dirname}"/uni0049.svg
-    touch "${dirname}"/uni004f.svg
-    touch "${dirname}"/uni0055.svg
-    touch "${dirname}"/uni0057.svg
+    touch "${dirname}"/uni0041.svg # A
+    touch "${dirname}"/uni0045.svg # E
+    touch "${dirname}"/uni0049.svg # I
+    touch "${dirname}"/uni004f.svg # O
+    touch "${dirname}"/uni0055.svg # U
+    touch "${dirname}"/uni0057.svg # W
     echo "ディレクトリ${dirname}の母音符号のタイムスタンプを更新しました。"
 done
