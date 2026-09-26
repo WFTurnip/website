5f3cdb41d26867ddbfeab92f0944b80271b07165
WFTurnip <tomohiro.kabura@icloud.com>
2026-09-26 13:12:44 +0900

    update

--- creative/fonts/brakets_maker.sh
+++ creative/fonts/brakets_maker.sh
@@ -2,13 +2,11 @@
 fontname=("kodito" "lekuta" "lozegw" "makina" "piswpi" "polwgo" "silwki" "sulive" "xavani" "xesada" "xidili" "zosokw")
 for dirname in "${fontname[@]}"; do
     echo "ディレクトリ${dirname}の括弧文字のタイムスタンプを更新します。"
-    touch "${dirname}"/uni003c.svg
-    touch "${dirname}"/uni003e.svg
-    touch "${dirname}"/uni0028.svg
-    touch "${dirname}"/uni0029.svg
-    touch "${dirname}"/uni005b.svg
-    touch "${dirname}"/uni005d.svg
-    touch "${dirname}"/uni007b.svg
-    touch "${dirname}"/uni007d.svg
+    touch "${dirname}"/uni0028.svg # (
+    touch "${dirname}"/uni0029.svg # )
+    touch "${dirname}"/uni005b.svg # [
+    touch "${dirname}"/uni005d.svg # ]
+    touch "${dirname}"/uni007b.svg # {
+    touch "${dirname}"/uni007d.svg # }
     echo "ディレクトリ${dirname}の括弧文字のタイムスタンプを更新しました。"
 done
--- creative/fonts/punctuations_concatenator.sh
+++ creative/fonts/punctuations_concatenator.sh
@@ -2,8 +2,8 @@
 echo "結合するフォントのディレクトリ名を入力してください。"
 ls -d */
 read -p ">>> " dirname
-cat "${dirname}"/uni0030.svg "${dirname}"/uni002f.svg >"${dirname}"/uni0025.svg                          #
-cat "${dirname}"/uni0051.svg "${dirname}"/uni003e.svg "${dirname}"/uni0027.svg >"${dirname}"/uni0026.svg #
-cat "${dirname}"/uni002c.svg "${dirname}"/uni0030.svg >"${dirname}"/uni003b.svg                          #
-cat "${dirname}"/uni002e.svg "${dirname}"/uni0030.svg >"${dirname}"/uni003a.svg                          #
-cat "${dirname}"/uni0052.svg "${dirname}"/uni0030.svg "${dirname}"/uni005c.svg >"${dirname}"/uni0040.svg #
+cat "${dirname}"/uni0030.svg "${dirname}"/uni002f.svg >"${dirname}"/uni0025.svg                          # CONCATENATE 0 / > %
+cat "${dirname}"/uni0051.svg "${dirname}"/uni003e.svg "${dirname}"/uni0027.svg >"${dirname}"/uni0026.svg # CONCATENATE Q > ' > &
+cat "${dirname}"/uni002c.svg "${dirname}"/uni0030.svg >"${dirname}"/uni003b.svg                          # CONCATENATE , 0 > ;
+cat "${dirname}"/uni002e.svg "${dirname}"/uni0030.svg >"${dirname}"/uni003a.svg                          # CONCATENATE . 0 > :
+cat "${dirname}"/uni0052.svg "${dirname}"/uni0030.svg "${dirname}"/uni005c.svg >"${dirname}"/uni0040.svg # CONCATENATE R 0 \\ > @
